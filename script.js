37bbb4124f7c0e17906871f35d9be7bee6fc8eb8
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-11-30 12:00:21 +0100

    done with first and second pages

--- script.js
+++ script.js
@@ -12,36 +12,49 @@ const addBudget = () => {
 
     budgetArray.push(proObject);
     localStorage.setItem("budget", JSON.stringify(budgetArray));
-    window.location.href = "trackIt.html"
-  }else{
+    window.location.href = "trackIt.html";
+  } else {
     display.innerHTML = `<p class="alert alert-danger w-100 col-lg col-sm col-md">fill the input</p>`;
   }
 };
 
 console.log(budgetArray);
 
-
 // result page
 
 let totalAmountSpent = 0;
 
-
 const displayAll = () => {
   // clear the card
-  resultDiv.innerHTML = ""
-  budgetArray.map((product, i)=>{
-    let productCost = product.proQuant * product.proPrice
-    totalAmountSpent += productCost
+  resultDiv.innerHTML = "";
+  budgetArray.map((product, i) => {
+    let productCost = product.proQuant * product.proPrice;
+    totalAmountSpent += productCost;
     resultDiv.innerHTML += `
-    ${product.proName}
-    ${product.proQuant}
-    ${product.proPrice}
-
-
     
+
+
+    <div class="card p-2">
+  <div class="first-content">
+    <span>    ${product.proName}
+    </span>
+  </div>
+  <div class="second-content d-flex flex-column text-white fs-5">
+<div>Quantity: ${product.proQuant}</div>
+<div>
+ Price: $${product.proPrice}
+</div>
+  </div>
+    <div class="d-flex justify-content-between">
+
     <button onclick="deleteAny(${i})" class="btn btn-danger text-light fw-bold" >delete</button>
     <a href="#" class="btn btn-warning text-light fw-bold" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal1-${i}">Edit</a>
     </div>
+    </div>
+</div>
+
+
+   
 
     <!-- Modal -->
     <div class="modal fade" id="exampleModal1-${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -65,48 +78,62 @@ const displayAll = () => {
       </div>
     </div>
     </div>`;
-    
-    
-    
-  })
-}
+  });
+};
 
-displayAll()
-
+displayAll();
 
 if (budgetArray.length > 0) {
-  document.getElementById("resultDiv").innerHTML += `<p class="text-light fw-bold bg-success p-2 text-center w-100 ">Total Budget: &#8358;${totalAmountSpent.toFixed(2)}</p>`;
+  document.getElementById(
+    "resulthorn"
+  ).innerHTML += `<p class="text-light fw-bold bg-info p-2 text-center w-100 pbottom">Total Budget: &#8358;${totalAmountSpent.toFixed(
+    2
+  )}</p>`;
 } else {
-  document.getElementById("resultDiv").innerHTML += `<h2 class="fw-bold textColor">No item added</h2>`;
+  document.getElementById(
+    "resultDiv"
+  ).innerHTML += `<h2 class="fw-bold textColor text-center">No item added</h2>`;
 }
 
 const editAny = (i) => {
   budgetArray[i]["proName"] = document.getElementById(`productName-${i}`).value;
-  budgetArray[i]["proQuant"] = document.getElementById(`quantityOwn-${i}`).value;
+  budgetArray[i]["proQuant"] = document.getElementById(
+    `quantityOwn-${i}`
+  ).value;
   budgetArray[i]["proPrice"] = document.getElementById(`priceName-${i}`).value;
   localStorage.setItem("budget", JSON.stringify(budgetArray));
-  totalSpent = 0; 
+  totalSpent = 0;
   resultCard.innerHTML = "";
   displayAll();
   if (budgetArray.length > 0) {
-    document.getElementById("resultDiv").innerHTML += `<p class="text-light fw-bold bg-success p-2 text-center w-100">Total Budget: &#8358;${totalAmountSpent.toFixed(2)}</p>`;
+    document.getElementById(
+      "resulthorn"
+    ).innerHTML += `<p class="text-light fw-bold bg-info p-2 text-center w-100 pbottom">Total Budget: &#8358;${totalAmountSpent.toFixed(
+      2
+    )}</p>`;
   }
 };
 
-
 const deleteAny = (i) => {
+  errorx.innerHTML = `<p class="alert alert-success known">Deleted successfully</p>`;
   budgetArray.splice(i, 1);
   localStorage.setItem("budget", JSON.stringify(budgetArray));
-  totalSpent = 0; 
-  resultDiv.innerHTML = ""; 
+  totalSpent = 0; // Reset totalSpent
+  resultDiv.innerHTML = ""; // Clear the card display
   displayAll(); // Redisplay the updated cards
   if (budgetArray.length > 0) {
-    document.getElementById("resultDiv").innerHTML += `<p class="text-light fw-bold bg-success p-2 text-center w-100">Total Budget: &#8358;${totalAmountSpent.toFixed(2)}</p>`;
+    document.getElementById(
+      "resulthorn"
+    ).innerHTML += `<p class="text-light fw-bold bg-success p-2 text-center w-100 pbottom">Total Budget: &#8358;${totalSpent.toFixed(
+      2
+    )}</p>`;
   } else {
-    resultDiv.innerHTML = ""
-    document.getElementById("resultDiv").innerHTML += `<h2 class="fw-bold textColor">No item added</h2>`;
+    resulthorn.innerHTML = "";
+    document.getElementById(
+      "resultDiv"
+    ).innerHTML += `<h2 class="fw-bold textColor">No item added</h2>`;
   }
-  // setTimeout(()=>{
-  //   errorx.style.display = "none"
-  // }, 2000)
+  setTimeout(() => {
+    errorx.style.display = "none";
+  }, 2000);
 };
